9ac039b1793a35dec8fc8adb35dd87e555a04408
Byron Blank <byronbla@gmail.com>
2024-04-11 18:44:47 -0400

    latest code

--- neostrip/neostrip.ino
+++ neostrip/neostrip.ino
@@ -1,35 +1,49 @@
 #include <Adafruit_NeoPixel.h>
+#include <normpattern.h>
 #include <fallpattern.h>
 #include <julypattern.h>
+#include <xmaspattern.h>
+#include <alertpattern.h>
 #define PIN 6
 
-
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(90, PIN, NEO_GRB + NEO_KHZ800);
-FallPattern fp(strip, 90, PIN);
+NormPattern np(strip, 90, PIN);
 JulyPattern jp(strip, 90, PIN);
+FallPattern fp(strip, 90, PIN);
+XmasPattern cp(strip, 90, PIN);
+AlertPattern ap(strip, 90, PIN);
 
-void setup() {
+void setup()
+{
   strip.begin();
   strip.setBrightness(255);
   strip.show();
   Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
 
-  if (Serial.available() > 0) {
+  if (Serial.available() > 0)
+  {
     // read the incoming byte:
     char incomingByte = Serial.read();
-  
-  switch(incomingByte) {
-    case 'f': 
-      fp.start();
+
+    switch (incomingByte)
+    {
+    case 'n':
+      np.start();
     case 'j':
       jp.start();
+    case 'f':
+      fp.start();
+    case 'c':
+      cp.start();
+    case 'a':
+      ap.start();
     case 'z':
       strip.clear();
-      strip.show();;
-  }
-  
+      strip.show();
+    }
   }
 }
